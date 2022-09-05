c652ce25a8a61d5359aaa73f93ba06ccce3a8596
AppleMac0315 <109144464+Applemac0315@users.noreply.github.com>
2022-09-05 21:54:45 +0400

    Doing this alone

---
+++ config.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+var path = require('path')
+
+module.exports = {
+  level: {
+    location: process.env.DB_PATH || path.join(__dirname, './db')
+  },
+  authentic: {
+    host: process.env.AUTHENTIC_HOST || 'https://ix-id.lincx.la'
+  }
+}
---
+++ test/index.js
@@ -0,0 +1,4 @@
+process.env.NODE_ENV = 'test'
+
+require('./routes')
+require('./models')
